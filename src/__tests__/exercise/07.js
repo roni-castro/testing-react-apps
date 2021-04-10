08a371e8b8d130015e5b662991bd1166926f5256
Roni <roni.c.castro@gmail.com>
2021-04-10 20:30:17 -0300

    feat: add test to the dark theme

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -18,3 +18,16 @@ test('renders with the light styles for the light theme', () => {
     color: black;
   `)
 })
+
+test('renders with the dark styles for the dark theme', () => {
+  render(
+    <ThemeProvider initialTheme="dark">
+      <EasyButton>Easy</EasyButton>
+    </ThemeProvider>,
+  )
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
+})
