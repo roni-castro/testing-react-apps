f07d45514c7ac658c670ce9485f5ded83899f984
Roni <roni.c.castro@gmail.com>
2021-04-10 20:44:58 -0300

    feat: add wrapper with them provider

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -7,11 +7,10 @@ import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
 test('renders with the light styles for the light theme', () => {
-  render(
-    <ThemeProvider>
-      <EasyButton>Easy</EasyButton>
-    </ThemeProvider>,
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme="light">{children}</ThemeProvider>
   )
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
@@ -20,11 +19,10 @@ test('renders with the light styles for the light theme', () => {
 })
 
 test('renders with the dark styles for the dark theme', () => {
-  render(
-    <ThemeProvider initialTheme="dark">
-      <EasyButton>Easy</EasyButton>
-    </ThemeProvider>,
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
   )
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: black;
