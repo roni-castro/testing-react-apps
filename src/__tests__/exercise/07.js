1a2b046ac48068b4ad5169fd960e5938b24bd857
Roni <roni.c.castro@gmail.com>
2021-04-10 21:53:34 -0300

    fix: import

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -2,8 +2,7 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {screen} from '@testing-library/react'
-import {render} from 'test/test-utils'
+import {render, screen} from 'test/test-utils'
 import EasyButton from '../../components/easy-button'
 
 test('renders with the light styles for the light theme', () => {
